5380660d05c387b73262b3c6a67031eabd1b5cfe
BenjikQ <benjik1656@gmail.com>
2023-02-15 13:34:49 +0100

    Fix not compiling when using clang
    Under Windows clang needs to link to pthread library.
    Additionally it crashed when the generator was initialized in cpp file
    not in header.
    
    * Find Thread package and link to it
    * Move generator definition to header file

--- src/utils/random.cpp
+++ src/utils/random.cpp
@@ -1,9 +1,5 @@
 #include "random.hpp"
 
-#include <random>
-
-static thread_local std::mt19937 gen{ std::random_device{}() };
-
 double random(double min, double max) {
     std::uniform_real_distribution<double> dist{ min, max };
     return dist(gen);
--- src/utils/random.hpp
+++ src/utils/random.hpp
@@ -1,6 +1,10 @@
 #ifndef RAYTRACINGINONEWEEKEND_UTILS_RANDOM_HPP
 #define RAYTRACINGINONEWEEKEND_UTILS_RANDOM_HPP
 
+#include <random>
+
+static std::mt19937 gen{ std::random_device{}() };
+
 [[nodiscard]] double random(double min, double max);
 
 #endif //RAYTRACINGINONEWEEKEND_UTILS_RANDOM_HPP
